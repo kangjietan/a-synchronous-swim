bf55dbf56de1f23697ce92e7b128c44aa89ea0f4
Hack Reactor Student <student@hackreactor.com>
2020-01-04 10:22:42 -0800

    Send back data from server

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
+const msgQueue = require('./messageQueue');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
@@ -14,16 +15,25 @@ module.exports.initialize = (queue) => {
 
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  res.writeHead(200, headers);
   if (req.method === 'OPTIONS') {
+    res.writeHead(200, headers);
     res.end();
     next();
   } else {
-    var moves = ['left', 'right', 'up', 'down'];
-    var randomIdx = Math.floor(Math.random() * 4);
-    var move = moves[randomIdx]
-    res.write(move);
-    res.end();
+    // var moves = ['left', 'right', 'up', 'down'];
+    // var randomIdx = Math.floor(Math.random() * 4);
+    // var move = moves[randomIdx]
+    // Check to make sure it is not undefined
+    var message = msgQueue.dequeue();
+    if (message !== undefined) {
+      console.log(msgQueue);
+      res.writeHead(200, headers);
+      res.write(message);
+      res.end();
+    } else {
+      res.writeHead(204, headers);
+      res.end();
+    }
     next(); // invoke next() at the end of a request to help with testing!
   }
 };
